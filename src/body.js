d2943d4cfd911d6419f014f0d8404a1efcedd41c
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-11 14:38:26 +0500

    Update body.js

--- src/body.js
+++ src/body.js
@@ -7,12 +7,12 @@ function Body() {
     return (
       <div className="App ${light=='OFF' ? 'OFF' : 'ON'}">
 
-          let [light,setLight]=useState('OFF')
+          let [light,setLight]= useState('OFF');
        
          Light Status = light
 
-         <button onClick={()=>{setLight('ON')}}>ON</button>
-         <button onClick={()=>{setLight('OFF')}}>OFF</button>
+         <button onClick={() => setLight('ON')}>ON</button>
+         <button onClick={()=> setLight('OFF')}>OFF</button>
          
        
       </div>
