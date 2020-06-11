4cb902a8bf5d1a300b62b41c8b6b4ad01445373e
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-11 14:28:37 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import BODY from './body.js'
+import Body from './body';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App /><Body />
-  </React.StrictMode>,
+     <div>
+    <App /> <Body />
+    </div>,
   document.getElementById('root')
 );
 
