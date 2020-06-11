a47fea3a08b8dad929c325162136116501384701
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-11 14:17:39 +0500

    Added body component
    status of light and buttons to change it are added

---
+++ src/body.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react';
+import OFF from './light.css';
+import ON from './light.css';
+
+
+function Body() {
+    return (
+      <div className="App ${light=='OFF' ? 'OFF' : 'ON'}">
+
+          let [light,setLight]=useState('OFF')
+       
+         Light Status = light
+
+         <button onClick={()=>{setLight('ON')}}>ON</button>
+         <button onClick={()=>{setLight('OFF')}}>OFF</button>
+         
+       
+      </div>
+    );
+  }
+  
+  export default Body;
+  
