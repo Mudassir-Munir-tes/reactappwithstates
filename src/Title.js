6b77e5903ac5c9ed96cebd3972d3bb80f4577ffe
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-11 13:34:04 +0500

    updated app.js
    created Title.js component and used it in App.js to show title

---
+++ src/Title.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function Title() {
+    return (
+      <div className="App">
+       
+         <h1>BOOTCAMP SESSION 04</h1>
+         <h1>ASSIGNMENT 04</h1>
+         <br></br>
+          
+         
+       
+      </div>
+    );
+  }
+  
+  export default Title;
+  
